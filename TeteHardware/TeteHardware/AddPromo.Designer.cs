fd9d5ae8d90eb75d244a6c89d21f7495c909ced1
ljcnavarro <ljcnavarro@JF610B-021>
2017-08-07 17:00:54 +0800

    Promov2

--- TeteHardware/TeteHardware/AddPromo.Designer.cs
+++ TeteHardware/TeteHardware/AddPromo.Designer.cs
@@ -28,11 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboDstatus = new System.Windows.Forms.ComboBox();
+            this.labelDstatus = new System.Windows.Forms.Label();
+            this.labelDvalue = new System.Windows.Forms.Label();
+            this.txtDvalue = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.labelDpercent = new System.Windows.Forms.Label();
+            this.labelDname = new System.Windows.Forms.Label();
+            this.labelDid = new System.Windows.Forms.Label();
+            this.txtDpercent = new System.Windows.Forms.TextBox();
+            this.txtDname = new System.Windows.Forms.TextBox();
+            this.txtDid = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboDstatus
+            // 
+            this.comboDstatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboDstatus.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboDstatus.FormattingEnabled = true;
+            this.comboDstatus.Location = new System.Drawing.Point(237, 237);
+            this.comboDstatus.Name = "comboDstatus";
+            this.comboDstatus.Size = new System.Drawing.Size(134, 25);
+            this.comboDstatus.TabIndex = 154;
+            // 
+            // labelDstatus
+            // 
+            this.labelDstatus.AutoSize = true;
+            this.labelDstatus.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDstatus.ForeColor = System.Drawing.Color.White;
+            this.labelDstatus.Location = new System.Drawing.Point(92, 243);
+            this.labelDstatus.Name = "labelDstatus";
+            this.labelDstatus.Size = new System.Drawing.Size(104, 19);
+            this.labelDstatus.TabIndex = 152;
+            this.labelDstatus.Text = "Discount Status:";
+            // 
+            // labelDvalue
+            // 
+            this.labelDvalue.AutoSize = true;
+            this.labelDvalue.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDvalue.ForeColor = System.Drawing.Color.White;
+            this.labelDvalue.Location = new System.Drawing.Point(92, 207);
+            this.labelDvalue.Name = "labelDvalue";
+            this.labelDvalue.Size = new System.Drawing.Size(104, 19);
+            this.labelDvalue.TabIndex = 153;
+            this.labelDvalue.Text = "Discount Value:";
+            // 
+            // txtDvalue
+            // 
+            this.txtDvalue.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDvalue.Location = new System.Drawing.Point(237, 202);
+            this.txtDvalue.Name = "txtDvalue";
+            this.txtDvalue.Size = new System.Drawing.Size(134, 25);
+            this.txtDvalue.TabIndex = 151;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(152)))));
+            this.btnClear.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(152)))));
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(220)))), ((int)(((byte)(209)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(293, 283);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(78, 29);
+            this.btnClear.TabIndex = 147;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // labelDpercent
+            // 
+            this.labelDpercent.AutoSize = true;
+            this.labelDpercent.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDpercent.ForeColor = System.Drawing.Color.White;
+            this.labelDpercent.Location = new System.Drawing.Point(92, 174);
+            this.labelDpercent.Name = "labelDpercent";
+            this.labelDpercent.Size = new System.Drawing.Size(113, 19);
+            this.labelDpercent.TabIndex = 148;
+            this.labelDpercent.Text = "Discount Percent:";
+            // 
+            // labelDname
+            // 
+            this.labelDname.AutoSize = true;
+            this.labelDname.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDname.ForeColor = System.Drawing.Color.White;
+            this.labelDname.Location = new System.Drawing.Point(92, 138);
+            this.labelDname.Name = "labelDname";
+            this.labelDname.Size = new System.Drawing.Size(106, 19);
+            this.labelDname.TabIndex = 149;
+            this.labelDname.Text = "Discount Name:";
+            // 
+            // labelDid
+            // 
+            this.labelDid.AutoSize = true;
+            this.labelDid.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDid.ForeColor = System.Drawing.Color.White;
+            this.labelDid.Location = new System.Drawing.Point(92, 103);
+            this.labelDid.Name = "labelDid";
+            this.labelDid.Size = new System.Drawing.Size(81, 19);
+            this.labelDid.TabIndex = 150;
+            this.labelDid.Text = "Discount ID:";
+            // 
+            // txtDpercent
+            // 
+            this.txtDpercent.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDpercent.Location = new System.Drawing.Point(237, 168);
+            this.txtDpercent.Name = "txtDpercent";
+            this.txtDpercent.Size = new System.Drawing.Size(134, 25);
+            this.txtDpercent.TabIndex = 144;
+            // 
+            // txtDname
+            // 
+            this.txtDname.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDname.Location = new System.Drawing.Point(237, 133);
+            this.txtDname.Name = "txtDname";
+            this.txtDname.Size = new System.Drawing.Size(134, 25);
+            this.txtDname.TabIndex = 142;
+            // 
+            // txtDid
+            // 
+            this.txtDid.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDid.Location = new System.Drawing.Point(237, 98);
+            this.txtDid.Name = "txtDid";
+            this.txtDid.ReadOnly = true;
+            this.txtDid.Size = new System.Drawing.Size(134, 25);
+            this.txtDid.TabIndex = 143;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(152)))));
+            this.btnUpdate.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(152)))));
+            this.btnUpdate.FlatAppearance.BorderSize = 0;
+            this.btnUpdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(220)))), ((int)(((byte)(209)))));
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(209, 283);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(78, 29);
+            this.btnUpdate.TabIndex = 146;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(152)))));
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(152)))));
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(220)))), ((int)(((byte)(209)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(125, 283);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(78, 29);
+            this.btnAdd.TabIndex = 145;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // AddPromo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 410);
+            this.Controls.Add(this.comboDstatus);
+            this.Controls.Add(this.labelDstatus);
+            this.Controls.Add(this.labelDvalue);
+            this.Controls.Add(this.txtDvalue);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.labelDpercent);
+            this.Controls.Add(this.labelDname);
+            this.Controls.Add(this.labelDid);
+            this.Controls.Add(this.txtDpercent);
+            this.Controls.Add(this.txtDname);
+            this.Controls.Add(this.txtDid);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "AddPromo";
             this.Text = "AddPromo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboDstatus;
+        private System.Windows.Forms.Label labelDstatus;
+        private System.Windows.Forms.Label labelDvalue;
+        private System.Windows.Forms.TextBox txtDvalue;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label labelDpercent;
+        private System.Windows.Forms.Label labelDname;
+        private System.Windows.Forms.Label labelDid;
+        private System.Windows.Forms.TextBox txtDpercent;
+        private System.Windows.Forms.TextBox txtDname;
+        private System.Windows.Forms.TextBox txtDid;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
